9b7067fffb6112b66e949586b4e8bcd91a379167
Chris Brown <chrisjosbrown@gmail.com>
2016-03-24 15:44:00 +0100

    refactor(): use polyfill service

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 var ConcatSource = require("webpack-core/lib/ConcatSource");
+var PolyfillService = require('polyfill-service');
 var fs = require("fs");
 
-function PolyfillsPlugin(polyfills) {
-    this.polyfills = polyfills || [];
-}
+module.exports = function PolyfillsPlugin(options) {
 
-module.exports = PolyfillsPlugin;
+	options = options || {};
+    options = {
+		uaString: 'Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)',
+    	minify: true
+    };
 
-PolyfillsPlugin.prototype.apply = function(compiler) {
+    return { apply: function (compiler) {
 
-    var filesContent = "";
-
-    this.polyfills.forEach(function(polyfill) {
-        var filePath = require.resolve("./polyfills/" + polyfill + "/polyfill");
-        if (filePath) {
-            filesContent += fs.readFileSync(filePath, { encoding: 'utf8' });
-        }
-    });
-
-    compiler.plugin("compilation", function(compilation) {
-        compilation.plugin("optimize-chunk-assets", function(chunks, callback) {
-            chunks.forEach(function(chunk) {
-                if(!chunk.initial) return;
-                chunk.files.forEach(function(file, i) {
-                    compilation.assets[file] = new ConcatSource("/* Polyfills */\n", filesContent, compilation.assets[file]);
-                });
-            });
-            callback();
-        });
-    });
+		PolyfillService.getPolyfillString(this.options).then(function(bundleString) {
+			compiler.plugin("compilation", function(compilation) {
+		        compilation.plugin("optimize-chunk-assets", function(chunks, callback) {
+		            chunks.forEach(function(chunk) {
+		                if(!chunk.initial) return;
+		                chunk.files.forEach(function(file, i) {
+		                    compilation.assets[file] = new ConcatSource("/* Polyfills */\n", bundleString, compilation.assets[file]);
+		                });
+		            });
+		            callback();
+		        });
+		    });
+		});
+    } };
 };
